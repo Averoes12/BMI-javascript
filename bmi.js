fdafb8a5db64ff60ac32a5c65d7e9e3ac86aeb9d
averoes12 <hz.ir.daff@gmail.com>
2019-10-16 07:45:51 +0700

    make body mass index with javascript

---
+++ bmi.js
@@ -0,0 +1,17 @@
+function bmi(bb, tb) {
+	var b = bb / (tb/100 * tb/100);
+	console.log(b);
+	if (b < 17 || b < 18.4){
+		return "Kurus";
+	}else if (b === 18.5 || b < 25.0 ) {
+		return "Normal";
+	}else if(b === 25.1 || b < 27){
+		return "Gemuk";
+	}else if (b > 27){
+	return "Obesitas";
+	}else {
+		return "Data Tidak Valid"
+	}
+}
+
+console.log(bmi(52, 170));
